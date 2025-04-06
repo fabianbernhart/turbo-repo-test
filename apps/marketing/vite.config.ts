8c2244e977093160798fe10115a7e2143e95a0a6
farbian_2005 <fabian_bernhart@web.de>
2025-04-06 14:46:07 +0200

    chore: configure monorepo aliases

--- apps/marketing/vite.config.ts
+++ apps/marketing/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@ui": path.resolve(__dirname, "../../packages/ui"),
+      "@hooks": path.resolve(__dirname, "../../packages/hooks"),
+      "@shared": path.resolve(__dirname, "../../shared"),
+    },
+  },
+});
