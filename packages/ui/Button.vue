635a453ece62a3cfc5bf2fa47ba3e3675c7ad5ce
farbian_2005 <fabian_bernhart@web.de>
2025-04-06 14:46:19 +0200

    chore: create ui base package

---
+++ packages/ui/Button.vue
@@ -0,0 +1,11 @@
+<!-- packages/ui/Button.vue -->
+<template>
+    <button class="btn">
+        <slot />
+    </button></template>
+<script lang="ts" setup></script>
+<style>
+.btn {
+    padding: 0.5rem;
+}
+</style>
